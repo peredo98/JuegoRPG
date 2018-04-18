da4670412bd1deef6375a9b52a0a9bf8a3b8c4a7
peredo98 <36453392+peredo98@users.noreply.github.com>
2018-04-18 13:00:58 -0500

    limitar el mapa

--- Map.java
+++ Map.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class Map extends GameObject{
 	private BufferedImage img = null;
-	
+
 	public Map(int x, int y, ID id, String url){
 		super(x, y, id, url);
 		setImgx(48);
@@ -15,8 +15,19 @@ public class Map extends GameObject{
 
 
 	public void tick(){
+
+		
 		x += velx;
 		y += vely;
+
+		//limitar el mapa
+
+		if((x < -881 || x > 286) || (y < -589 || y > 152) || (x < -818 && y < -274) || (x < -245 && y > -91) || (x > 97 && y < -79) || (x > 46 && y < -415) || (x > -158 && x < 76 && y > -91) || (x > -797 && x < -242 && y > -544 && y < -211)){
+		x -= velx;
+		y -= vely;
+		}
+		
+
 	}
 
 	public void render(Graphics g){
@@ -28,5 +39,11 @@ public class Map extends GameObject{
 		g.drawImage(img, x, y, 1280, 960, null);
 	}
 
+	/*
+	if((x >= -881 && x <= 94 && y >= -208 && y <= -106) || ()){
+				x += velx;
+				y += vely;
+			
+	 */
 
 }
